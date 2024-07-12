74e6392049099babfdb9b2b03c563bd871456b86
27Cobalter <kontyanzhihg+github@gmail.com>
2024-07-13 06:02:56 +0900

    [add] vhadd impl

---
+++ src/vhadd/main.cc
@@ -0,0 +1,178 @@
+#include <cassert>
+#include <print>
+#include <chrono>
+#include <concepts>
+#include <format>
+#include <iostream>
+#include <limits>
+#include <new>
+#include <numeric>
+#include <random>
+#include <ranges>
+#include <span>
+#include <valarray>
+
+#include <opencv2/opencv.hpp>
+
+#include <InstructionInfo.h>
+
+#include "vhadd.h"
+
+template <typename T, typename U>
+  requires std::convertible_to<T, float> && std::convertible_to<U, float>
+float CalcDiff(T* t, U* u, int32_t data_size) {
+  float diff = 0;
+  for (auto i : std::views::iota(0, data_size)) {
+    diff += (t[i] - u[i]) * (t[i] - u[i]);
+    if (std::abs(static_cast<float>(t[i] - u[i])) > 0.1) {
+      // std::cout << std::format("(t[{}], u[{}]) = ({}, {})", i, i, t[i], u[i]) << std::endl;
+    }
+  }
+  return diff / data_size;
+}
+
+auto main() -> int {
+#ifdef _MSC_VER
+  constexpr int32_t loop_count = 10000;
+  std::valarray<int32_t> width_samples{1024};
+#else
+  constexpr int32_t loop_count = 1000;
+  std::valarray<int32_t> width_samples{1024};
+#endif
+
+  using IIIS                      = InstructionInfo::InstructionSet;
+  const bool supported_avx2       = InstructionInfo::IsSupported(IIIS::AVX2);
+  const bool supported_avx512f    = InstructionInfo::IsSupported(IIIS::AVX512F);
+  const bool supported_avx512vbmi = InstructionInfo::IsSupported(IIIS::AVX512_VBMI);
+
+  std::chrono::high_resolution_clock::time_point start, end;
+  int32_t time_count;
+  float diff;
+
+  constexpr int32_t RANGE_MAX  = std::numeric_limits<uint16_t>::max();
+  constexpr int32_t RANGE_SIZE = RANGE_MAX + 1;
+
+  std::random_device seed;
+  std::mt19937 gen(seed());
+  std::normal_distribution<> norm(RANGE_SIZE >> 2, RANGE_SIZE / 9);
+
+  for (auto width : width_samples) {
+    cv::Mat src   = cv::Mat::zeros(cv::Size(width, width), CV_16UC1);
+    cv::Mat h_ref;
+    cv::Mat v_ref;
+
+    std::span<uint16_t> h_dst;
+    std::span<uint16_t> v_dst;
+
+    const int32_t data_size = width * width;
+
+    uint16_t* sptr  = src.ptr<uint16_t>(0);
+    for (auto j : std::views::iota(0, src.rows)) {
+      for (auto i : std::views::iota(0, src.cols)) {
+        sptr[j * width + i] = RANGE_MAX * static_cast<float>(i) / (width - 1);
+      }
+    }
+
+    VHAdd vhadd(src.cols, src.rows);
+
+    std::string title;
+    title = "reducce_v";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      cv::reduce(src, v_ref, 0, cv::REDUCE_AVG, CV_32F);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "reducce_h";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      cv::reduce(src, h_ref, 1, cv::REDUCE_AVG, CV_32F);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "reducce_vh";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      cv::reduce(src, v_ref, 0, cv::REDUCE_AVG, CV_32F);
+      cv::reduce(src, h_ref, 1, cv::REDUCE_AVG, CV_32F);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "CalcV Naive";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      v_dst = vhadd.CalcV_Impl<VHAdd::Method::Naive>(src.ptr<uint16_t>(0), src.cols * src.rows,
+                                                      0, 0, src.cols, src.rows);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "CalcH Naive";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      h_dst = vhadd.CalcH_Impl<VHAdd::Method::Naive>(src.ptr<uint16_t>(0), src.cols * src.rows,
+                                                      0, 0, src.cols, src.rows);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "CalcV AVX2";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      v_dst = vhadd.CalcV_Impl<VHAdd::Method::AVX2>(src.ptr<uint16_t>(0), src.cols * src.rows,
+                                                      0, 0, src.cols, src.rows);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "CalcH AVX2";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      h_dst = vhadd.CalcH_Impl<VHAdd::Method::AVX2>(src.ptr<uint16_t>(0), src.cols * src.rows,
+                                                      0, 0, src.cols, src.rows);
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    title = "CalcVH AVX2";
+    start = std::chrono::high_resolution_clock::now();
+    for (auto i : std::views::iota(0, loop_count)) {
+      auto [v_slice, h_slice] = vhadd.CalcVH_Impl<VHAdd::Method::AVX2>(src.ptr<uint16_t>(0), src.cols * src.rows,
+                                                      0, 0, src.cols, src.rows);
+
+      h_dst = h_slice;
+      v_dst = v_slice;
+    }
+    end = std::chrono::high_resolution_clock::now();
+    std::println("{}: {} us", title,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_count);
+
+    cv::imshow("src", src);
+    for (auto i : std::views::iota(0, h_ref.size().area())) {
+      std::println("[i]: {}, v: {}, h: {}, vd: {}, hd: {}", i,
+          v_ref.ptr<float>(0)[i], h_ref.ptr<float>(0)[i], v_dst[i], h_dst[i]
+      );
+    }
+    cv::waitKey();
+  }
+
+  return 0;
+}
---
+++ src/vhadd/vhadd_impl_avx2.cc
@@ -0,0 +1,147 @@
+#include "vhadd.h"
+
+#include <algorithm>
+#include <cassert>
+#include <ranges>
+
+#include <immintrin.h>
+
+constexpr VHAdd::Method VMA2 = VHAdd::Method::AVX2;
+template <>
+std::span<uint16_t> VHAdd::CalcV_Impl<VMA2>(uint16_t* src, int32_t size, int32_t offset_x,
+                                            int32_t offset_y, int32_t horizontal,
+                                            int32_t vertical) {
+  assert(width_ * height_ == size);
+  assert((offset_x + horizontal) <= width_);
+  assert((offset_y + vertical) <= height_);
+
+  std::span<int32_t> acc = std::span<int32_t>(vaptr_ + offset_x, horizontal);
+  result_slice_[0]       = std::span<uint16_t>(vdptr_ + offset_x, horizontal);
+  std::ranges::fill(acc, 0);
+
+  constexpr int32_t step      = 256 / 8 / sizeof(uint16_t);
+  constexpr int32_t half_step = step >> 1;
+
+  __m256i zero_v = _mm256_setzero_si256();
+  for (auto j : std::views::iota(offset_y, vertical)) {
+    uint16_t* sptrj = src + width_ * j;
+    for (int32_t i = offset_x; i < horizontal; i += step) {
+      int32_t* vaptri = vaptr_ + i;
+      __m256i spji    = _mm256_loadu_epi32(sptrj + i);
+      __m256i vailo   = _mm256_loadu_epi32(vaptri);
+      __m256i vaihi   = _mm256_loadu_epi32(vaptri + half_step);
+      __m256i lo      = _mm256_unpacklo_epi16(spji, zero_v);
+      __m256i hi      = _mm256_unpackhi_epi16(spji, zero_v);
+      lo              = _mm256_add_epi32(vailo, lo);
+      hi              = _mm256_add_epi32(vaihi, hi);
+      _mm256_storeu_epi32(vaptri, lo);
+      _mm256_storeu_epi32(vaptri + half_step, hi);
+    }
+  }
+
+  float r   = 1.0 / vertical;
+  __m256 rv = _mm256_set1_ps(r);
+  for (int32_t i = offset_x; i < horizontal; i += step) {
+    // _mm256_storeu_epi32(vdptr_ + i, _mm256_setzero_si256());
+    int32_t* vaptri = vaptr_ + i;
+    __m256 vailo    = _mm256_cvtepi32_ps(_mm256_loadu_epi32(vaptri));
+    __m256 vaihi    = _mm256_cvtepi32_ps(_mm256_loadu_epi32(vaptri + half_step));
+    __m256i lo      = _mm256_cvtps_epi32(_mm256_mul_ps(vailo, rv));
+    __m256i hi      = _mm256_cvtps_epi32(_mm256_mul_ps(vaihi, rv));
+    _mm256_storeu_epi32(vdptr_ + i, _mm256_packus_epi32(lo, hi));
+  }
+
+  return result_slice_[0];
+}
+
+template <>
+std::span<uint16_t> VHAdd::CalcH_Impl<VMA2>(uint16_t* src, int32_t size, int32_t offset_x,
+                                            int32_t offset_y, int32_t horizontal,
+                                            int32_t vertical) {
+  assert(width_ * height_ == size);
+  assert((offset_x + horizontal) <= width_);
+  assert((offset_y + vertical) <= height_);
+
+  result_slice_[1] = std::span<uint16_t>(hdptr_ + offset_y, vertical);
+
+  constexpr int32_t step = 256 / 8 / sizeof(uint16_t);
+
+  float r        = 1.0 / horizontal;
+  __m256i zero_v = _mm256_setzero_si256();
+  for (auto j : std::views::iota(offset_y, vertical)) {
+    uint16_t* sptrj = src + width_ * j;
+    __m256i acc     = _mm256_setzero_si256();
+    for (int32_t i = offset_x; i < horizontal; i += step) {
+      __m256i spji = _mm256_loadu_epi32(sptrj + i);
+      __m256i lo   = _mm256_unpacklo_epi16(spji, zero_v);
+      __m256i hi   = _mm256_unpackhi_epi16(spji, zero_v);
+      acc          = _mm256_add_epi32(acc, _mm256_add_epi32(lo, hi));
+    }
+    __m256i cross = _mm256_shuffle_epi32(acc, _MM_SHUFFLE(1, 0, 3, 2));
+    acc           = _mm256_add_epi32(acc, cross);
+    cross         = _mm256_shuffle_epi32(acc, _MM_SHUFFLE(2, 3, 0, 1));
+    acc           = _mm256_add_epi32(acc, cross);
+    int32_t v     = reinterpret_cast<int32_t*>(&acc)[0] + reinterpret_cast<int32_t*>(&acc)[4];
+    hdptr_[j]     = static_cast<uint16_t>(static_cast<float>(v) * r);
+  }
+
+  return result_slice_[1];
+}
+
+template <>
+std::array<std::span<uint16_t>, 2> VHAdd::CalcVH_Impl<VMA2>(uint16_t* src, int32_t size,
+                                                            int32_t offset_x, int32_t offset_y,
+                                                            int32_t horizontal,
+                                                            int32_t vertical) {
+  assert(width_ * height_ == size);
+  assert((offset_x + horizontal) <= width_);
+  assert((offset_y + vertical) <= height_);
+
+  std::span<int32_t> acc = std::span<int32_t>(vaptr_ + offset_x, horizontal);
+  result_slice_[0]       = std::span<uint16_t>(vdptr_ + offset_x, horizontal);
+  result_slice_[1]       = std::span<uint16_t>(hdptr_ + offset_y, vertical);
+  std::ranges::fill(acc, 0);
+
+  constexpr int32_t step      = 256 / 8 / sizeof(uint16_t);
+  constexpr int32_t half_step = step >> 1;
+
+  float hr       = 1.0 / horizontal;
+  __m256i zero_v = _mm256_setzero_si256();
+  for (auto j : std::views::iota(offset_y, vertical)) {
+    uint16_t* sptrj = src + width_ * j;
+
+    __m256i acc = _mm256_setzero_si256(); // h
+    for (int32_t i = offset_x; i < horizontal; i += step) {
+      int32_t* vaptri = vaptr_ + i;
+      __m256i spji    = _mm256_loadu_epi32(sptrj + i);
+      __m256i vailo   = _mm256_loadu_epi32(vaptri);             // v
+      __m256i vaihi   = _mm256_loadu_epi32(vaptri + half_step); // v
+      __m256i lo      = _mm256_unpacklo_epi16(spji, zero_v);
+      __m256i hi      = _mm256_unpackhi_epi16(spji, zero_v);
+      acc             = _mm256_add_epi32(acc, _mm256_add_epi32(lo, hi)); // h
+      lo              = _mm256_add_epi32(vailo, lo);                     // v
+      hi              = _mm256_add_epi32(vaihi, hi);                     // v
+      _mm256_storeu_epi32(vaptri, lo);                                   // v
+      _mm256_storeu_epi32(vaptri + half_step, hi);                       // v
+    }
+    __m256i cross = _mm256_shuffle_epi32(acc, _MM_SHUFFLE(1, 0, 3, 2));                    // h
+    acc           = _mm256_add_epi32(acc, cross);                                          // h
+    cross         = _mm256_shuffle_epi32(acc, _MM_SHUFFLE(2, 3, 0, 1));                    // h
+    acc           = _mm256_add_epi32(acc, cross);                                          // h
+    int32_t v = reinterpret_cast<int32_t*>(&acc)[0] + reinterpret_cast<int32_t*>(&acc)[4]; // h
+    hdptr_[j] = static_cast<uint16_t>(static_cast<float>(v) * hr);                         // h
+  }
+
+  float vr    = 1.0 / vertical;                                                   // v
+  __m256 vr_v = _mm256_set1_ps(vr);                                               // v
+  for (int32_t i = offset_x; i < horizontal; i += step) {                         // v
+    int32_t* vaptri = vaptr_ + i;                                                 // v
+    __m256 vailo    = _mm256_cvtepi32_ps(_mm256_loadu_epi32(vaptri));             // v
+    __m256 vaihi    = _mm256_cvtepi32_ps(_mm256_loadu_epi32(vaptri + half_step)); // v
+    __m256i lo      = _mm256_cvtps_epi32(_mm256_mul_ps(vailo, vr_v));             // v
+    __m256i hi      = _mm256_cvtps_epi32(_mm256_mul_ps(vaihi, vr_v));             // v
+    _mm256_storeu_epi32(vdptr_ + i, _mm256_packus_epi32(lo, hi));                 // v
+  }
+
+  return result_slice_;
+}
