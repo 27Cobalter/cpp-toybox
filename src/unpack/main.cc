c566d519a9b90fdbfb2c1f5d1f00afd3a70b4bf4
27Cobalter <kontyanzhihg+github@gmail.com>
2024-07-30 21:32:21 +0900

    [add]  mono12

--- src/unpack/main.cc
+++ src/unpack/main.cc
@@ -9,12 +9,13 @@
 auto main() -> int {
   constexpr int32_t img_size      = 2048 * 2048;
   constexpr int32_t loop_num      = 10000;
+  // constexpr int32_t loop_num      = 1;
   std::shared_ptr<uint8_t[]> ssp  = std::make_shared<uint8_t[]>(img_size / 2 * 3);
   std::shared_ptr<uint16_t[]> dsp = std::make_shared<uint16_t[]>(img_size);
   uint8_t* src                    = ssp.get();
   uint16_t* dst                   = dsp.get();
 
-  {
+  { // mono12p
     for (int i = 0; i < img_size / 2 * 3; i += 3) {
       uint16_t elem1 = 4000 + (2 * i / 3);
       uint16_t elem2 = 4000 + (2 * i / 3) + 1;
@@ -26,22 +27,50 @@ auto main() -> int {
     auto start = std::chrono::high_resolution_clock::now();
     for (auto loop : std::views::iota(0, loop_num)) {
       const __m256i shuffle_idx =
-          _mm256_setr_epi8(4, 5, 7, 8, 5, 6, 8, 9, 10, 11, 13, 14, 11, 12, 14, 15, 0, 1, 3, 4,
-                           1, 2, 4, 5, 6, 7, 9, 10, 7, 8, 10, 11);
-      const __m256i shift_v  = _mm256_set_epi32(4, 0, 4, 0, 4, 0, 4, 0);
+          _mm256_setr_epi8(4, 5, 5, 6, 7, 8, 8, 9, 10, 11, 11, 12, 13, 14, 14, 15, 0, 1, 1, 2,
+                           3, 4, 4, 5, 6, 7, 7, 8, 9, 10, 10, 11);
       const __m256i and_mask = _mm256_set1_epi16(0x0FFF);
-      const __m256i shuffle_idx2 =
-          _mm256_setr_epi8(0, 1, 4, 5, 2, 3, 6, 7, 8, 9, 12, 13, 10, 11, 14, 15, 0, 1, 4, 5, 2,
-                           3, 6, 7, 8, 9, 12, 13, 10, 11, 14, 15);
       int32_t si        = 0;
       int32_t di        = 0;
       int32_t end_count = img_size / 2 * 3;
       for (; si < end_count; si += 24, di += 16) {
         __m256i a = _mm256_loadu_epi8(src - 4 + si);
         __m256i b = _mm256_shuffle_epi8(a, shuffle_idx);
-        __m256i c = _mm256_srlv_epi32(b, shift_v);
-        __m256i d = _mm256_and_epi32(c, and_mask);
-        __m256i e = _mm256_shuffle_epi8(d, shuffle_idx2);
+        __m256i c = _mm256_srli_epi32(b, 4);
+        __m256i d = _mm256_blend_epi16(b, c, 0b1010101010101010);
+        __m256i e = _mm256_and_epi32(d, and_mask);
+        _mm256_storeu_epi16(dst + di, e);
+      }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
+                     loop_num
+              << std::endl;
+  }
+
+  { // mono12
+    for (int i = 0; i < img_size / 2 * 3; i += 3) {
+      uint16_t elem1 = 4000 + (2 * i / 3);
+      uint16_t elem2 = 4000 + (2 * i / 3) + 1;
+      src[i]         = (elem1 & 0xFF0) >> 4;
+      src[i + 1]     = (elem2 & 0x00F) << 4 | (elem1 & 0x00F);
+      src[i + 2]     = (elem2 & 0xFF0) >> 4;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (auto loop : std::views::iota(0, loop_num)) {
+      const __m256i shuffle_idx =
+          _mm256_setr_epi8(5, 4, 5, 6, 8, 7, 8,9, 11, 10, 11, 12, 14, 13, 14, 15, 1, 0, 1, 2, 4 ,3, 4, 5, 7, 6 ,7, 8, 10, 9, 10, 11);
+      const __m256i blend_mask = _mm256_set1_epi32(0x000000FF);
+      int32_t si        = 0;
+      int32_t di        = 0;
+      int32_t end_count = img_size / 2 * 3;
+      for (; si < end_count; si += 24, di += 16) {
+        __m256i a = _mm256_loadu_epi8(src - 4 + si);
+        __m256i b = _mm256_shuffle_epi8(a, shuffle_idx);
+        __m256i c = _mm256_slli_epi16(b, 4);
+        __m256i d = _mm256_blendv_epi8(b, c, blend_mask);
+        __m256i e = _mm256_srli_epi16(d, 4);
         _mm256_storeu_epi16(dst + di, e);
       }
     }
