408f84c6c8fdc55974f695f521126f41621d58ed
27Cobalter <kontyanzhihg+github@gmail.com>
2024-12-08 21:27:34 +0900

    Add Reader Writer

--- src/stdio/stdio.cc
+++ src/stdio/stdio.cc
@@ -1,18 +1,12 @@
-#include <iostream>
+#include <chrono>
 #include <fstream>
+#include <iostream>
 #include <ranges>
+#include <thread>
 
 auto main() -> int {
   constexpr int32_t buffer_size = 16;
   char buffer[buffer_size];
-
-  std::ofstream ofs("./bin.bin", std::ios::binary);
-  for (auto i : std::views::iota(0, 26)) {
-    char buf[1];
-    buf[0] = i;
-    ofs.write(buf, 1);
-  }
-  ofs.close();
 
   std::istream* is;
   std::ostream* os;
@@ -20,14 +14,19 @@ auto main() -> int {
   os = &std::cout;
 
   while (is->eof() == false) {
+    // auto read_size = is->readsome(buffer, buffer_size);
     is->read(buffer, buffer_size);
     auto read_size = is->gcount();
     std::cout << read_size << std::endl;
-    for(auto i : std::views::iota(0, read_size))
-    {
+    if (read_size == 0) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      continue;
+    }
+    for (auto i : std::views::iota(0, read_size)) {
       buffer[i] += 'A';
     }
     std::cout.write(buffer, read_size);
+    std::cout << std::endl;
   }
   std::cout << std::endl;
 
