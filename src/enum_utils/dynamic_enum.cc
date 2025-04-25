68b5df000f4e1f72abc6dc8e2b2a74e04278ee91
27Cobalter <kontyanzhihg+github@gmail.com>
2025-04-26 00:04:19 +0900

    use optional

--- src/enum_utils/dynamic_enum.cc
+++ src/enum_utils/dynamic_enum.cc
@@ -48,12 +48,13 @@ template <auto EV>
 struct GetName {
   static constexpr std::string_view name = ValueName<EV>();
   static constexpr auto value =
-      name != "" ? std::optional<std::optional<std::string_view>>(std::optional<std::string_view>(name)) : std::nullopt;
+      name != "" ? std::optional<std::string_view>(name) : std::nullopt;
 };
 
-template <typename T, uint32_t Min, uint32_t Max, template <auto> typename Predicate, size_t... Indices>
+template <typename T, uint32_t Min, uint32_t Max, template <auto> typename Predicate,
+          size_t... Indices>
 consteval auto create_filtered_list_impl(std::index_sequence<Indices...>) {
-  constexpr std::array values = {(Predicate<static_cast<T>(Min + Indices)>::value.value_or(std::nullopt))...};
+  constexpr std::array values = {(Predicate<static_cast<T>(Min + Indices)>::value)...};
 
   // 有効な値のみを抽出（-1は無効値としてフィルタリング）
   constexpr size_t count = ((values[Indices].has_value()) + ...);
@@ -68,7 +69,7 @@ consteval auto create_filtered_list_impl(std::index_sequence<Indices...>) {
   return result;
 }
 
-template<typename T, auto Min, auto Max, template <auto> typename Predicate>
+template <typename T, auto Min, auto Max, template <auto> typename Predicate>
 consteval auto create_filtered_list() {
   return create_filtered_list_impl<T, Min, Max, Predicate>(
       std::make_index_sequence<Max - Min + 1>{});
@@ -93,7 +94,7 @@ auto main() -> int32_t {
 
   constexpr auto filtered_list = create_filtered_list<Enum, 0x00000000, 0x00000100, GetName>();
   std::println("Filtered list size: {}", filtered_list.size());
-  for(const auto& e : filtered_list) {
+  for (const auto& e : filtered_list) {
     std::println("Filtered => {}", std::string(e));
   }
   return 0;
