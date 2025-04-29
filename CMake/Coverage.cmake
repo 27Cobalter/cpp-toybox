3926b15bc9f525a84d6dfae8919e75b5224d6b69
27Cobalter <kontyanzhihg+github@gmail.com>
2025-04-29 23:50:31 +0900

    Debug

--- CMake/Coverage.cmake
+++ CMake/Coverage.cmake
@@ -1,21 +1,27 @@
 find_program(GCOVR_EXECUTABLE gcovr)
 find_program(GCOV_EXECUTABLE gcov)
 
+message(STATUS "gcovr executable: ${GCOVR_EXECUTABLE}")
+message(STATUS "gcov executable: ${GCOV_EXECUTABLE}")
+
 if(NOT GCOVR_EXECUTABLE OR NOT GCOV_EXECUTABLE)
   message(WARNING "gcov not found")
   return()
 endif()
 
-set(CMAKE_CXX_FLAGS_COVERAGE
-    "-O0 -g --coverage -fprofile-abs-path -fprofile-exclude-files=\"build/*;test/*\""
+string(
+  APPEND
+  CMAKE_CXX_FLAGS_DEBUG
+  " -O0 -g --coverage -fprofile-abs-path -fprofile-exclude-files=\"build/*;test/*\""
 )
-set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "--coverage")
-set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "--coverage")
-set(CMAKE_MODULE_LINKER_FLAGS_COVERAGE "--coverage")
+string(APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG " --coverage")
+string(APPEND CMAKE_SHARED_LINKER_FLAGS_DEBUG " --coverage")
+string(APPEND CMAKE_MODULE_LINKER_FLAGS_DEBUG " --coverage")
 
 make_directory("${CMAKE_BINARY_DIR}/coverage")
 configure_file(${CMAKE_CURRENT_LIST_DIR}/gcovr.cfg.in gcovr.cfg @ONLY)
 
+message(STATUS "Add Target: coverage")
 add_custom_target(
   coverage
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
