d77a586df21f5fdb5c6172e2234e8a48b9a0e265
27Cobalter <kontyanzhihg+github@gmail.com>
2025-06-22 13:17:05 +0900

    add options

--- CMake/MemCheck.cmake
+++ CMake/MemCheck.cmake
@@ -14,16 +14,15 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/valgrind_wrapper.sh DESTINATION ${CMAKE_BINA
 set(CUSTOM_MEMORYCHECK_TYPE "MEMCHECK" CACHE STRING "")
 if(CUSTOM_MEMORYCHECK_TYPE STREQUAL "MEMCHECK")
       message(STATUS "Set MemoryCheckType: MEMCHECK")
-      set(MEMORYCHECK_COMMAND_OPTIONS
-      "--tool=memcheck --xml=yes --xml-file=${CMAKE_BINARY_DIR}/memcheck/\${REPLACE_TEST_NAME}.xml"
-      )
+      set(MEMORYCHECK_COMMAND_OPTIONS "--tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose")
+      string(APPEND MEMORYCHECK_COMMAND_OPTIONS " --xml=yes --xml-file=${CMAKE_BINARY_DIR}/memcheck/\${REPLACE_TEST_NAME}.xml")
 elseif(CUSTOM_MEMORYCHECK_TYPE STREQUAL "HELGRIND")
       message(STATUS "Set MemoryCheckType: HELGRIND")
-      set(MEMORYCHECK_COMMAND_OPTIONS
-      "--tool=helgrind --xml=yes --xml-file=${CMAKE_BINARY_DIR}/helgrind/\${REPLACE_TEST_NAME}.xml"
-      )
+      set(MEMORYCHECK_COMMAND_OPTIONS "--tool=helgrind --history-level=full --conflict-cache-size=10000000")
+       string(APPEND MEMORY_CHECK_COMMAND_OPTIONS " --xml=yes --xml-file=${CMAKE_BINARY_DIR}/helgrind/\${REPLACE_TEST_NAME}.xml")
 else()
       message(FATAL_ERROR "CUSTOM_MEMORYCHECK_TYPE Support MEMCHECK or HELGRIND")
 endif()
+string(APPEND MEMORY_CHECK_COMMAND_OPTIONS " --error-limt=no --max-threads=20 --max-stackframe=20000000 --main-stacksize=8000000 --num-callers=20")
 
 include(CTest)
