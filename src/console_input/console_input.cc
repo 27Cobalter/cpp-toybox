395ba626f03937af5c67dda3e3e315fa925ce586
27Cobalter <kontyanzhihg+github@gmail.com>
2025-10-13 17:58:38 +0900

    Add win

--- src/console_input/console_input.cc
+++ src/console_input/console_input.cc
@@ -5,12 +5,11 @@
 
 #include <conio.h>
 #else
-#include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
 #endif
 
 #include "console_input.h"
-
 
 #ifdef _WIN32
 ConsoleInput::ConsoleInput() {}
@@ -18,20 +17,32 @@ ConsoleInput::~ConsoleInput() {}
 
 std::optional<char> ConsoleInput::NonblockingGetChar() {
   if (_kbhit() == true) {
-    return static_cast<char>(_getch());
-  } else{
+    int32_t c = _getch();
+    if (c == 0 || c == 0xe0) {
+      std::println("");
+      std::print("[0] special ({:0x})", static_cast<int32_t>(c));
+      for (int32_t i = 0; _kbhit() == true; i++) {
+        int32_t c2;
+        c2 = _getch();
+        std::print(", [{}] {} ({:0x})", i, static_cast<char>(c2), static_cast<int32_t>(c2));
+      }
+      std::println("");
+      return std::nullopt;
+    }
+    return static_cast<char>(c);
+  } else {
     return std::nullopt;
   }
 }
 #else
 ConsoleInput::ConsoleInput() {
   termios* old = new termios();
-  context_ = reinterpret_cast<void*>(old);
+  context_     = reinterpret_cast<void*>(old);
 
   tcgetattr(STDIN_FILENO, old);
   termios tnew = *old;
   tnew.c_lflag &= ~(ICANON | ECHO);
-  tnew.c_cc[VMIN] = 0;
+  tnew.c_cc[VMIN]  = 0;
   tnew.c_cc[VTIME] = 0;
   tcsetattr(STDIN_FILENO, TCSANOW, &tnew);
 }
@@ -44,13 +55,11 @@ ConsoleInput::~ConsoleInput() {
 std::optional<char> ConsoleInput::NonblockingGetChar() {
   char c;
   if (read(STDIN_FILENO, &c, 1) == 1) {
-    if (c == '\x1b')
-    {
+    if (c == '\x1b') {
       std::println("");
-      std::print("[0] esc {:0x}", static_cast<int32_t>(c));
+      std::print("[0] esc ({:0x})", static_cast<int32_t>(c));
       char seq;
-      for (int32_t i = 0; read(STDIN_FILENO, &seq, 1) == 1; i++)
-      {
+      for (int32_t i = 0; read(STDIN_FILENO, &seq, 1) == 1; i++) {
         std::print(", [{}] {} ({:0x})", i, seq, static_cast<int32_t>(seq));
       }
       std::println("");
