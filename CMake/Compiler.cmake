6335bc07dcd6a72243c55b513765b8adb62290ca
27Cobalter <kontyanzhihg+github@gmail.com>
2025-10-25 01:25:39 +0900

    update

--- CMake/Compiler.cmake
+++ CMake/Compiler.cmake
@@ -4,9 +4,9 @@ set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 if(NOT CMAKE_CXX_COMPILER_ID MATCHES "MSVC") # GNU or Clang
       if (CMAKE_SYSTEM_PROCESSOR MATCHES "^(x86_64|amd64)$")
-            string(APPEND CMAKE_CXX_FLAGS " -march=x86-64-v3 -fcf-protection=full")
+            string(APPEND CMAKE_CXX_FLAGS " -march=x86-64-v3")
       elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^(aarch64|arm64)$")
-            string(APPEND CMAKE_CXX_FLAGS " -march=armv8-a -mbranch-protection=standard")
+            string(APPEND CMAKE_CXX_FLAGS " -march=armv8-a")
       endif()
 
       string(APPEND CMAKE_CXX_FLAGS "")
@@ -16,10 +16,10 @@ if(NOT CMAKE_CXX_COMPILER_ID MATCHES "MSVC") # GNU or Clang
 endif()
 
 if(NOT MSVC) # GNU like
-      string(APPEND CMAKE_CXX_FLAGS " -fvisibility=hidden -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wl,-Bsymbolic")
+      string(APPEND CMAKE_CXX_FLAGS " -pipe -fno-plt -fvisibility=hidden -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wl,-Bsymbolic")
       string(APPEND CMAKE_CXX_FLAGS_DEBUG " -g3 -fstack-protector-strong -fstack-clash-protection -D_GLIBCXX_ASSERTIONS -D_LIBCPP_ASSERT -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-common")
       string(APPEND CMAKE_CXX_FLAGS_RELWITHDEBINFO " -g3 -fstack-protector-strong -D_FORTIFY_SOURCE=3")
-      string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g0 -fno-stack-clash-protection -fno-stack-protector -fomit-frame-pointer -foptimize-sibling-calls -fcommon -flto=auto -s -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0")
+      string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g0 -funroll-loops -fno-stack-clash-protection -fno-stack-protector -fomit-frame-pointer -foptimize-sibling-calls -fcommon -flto=auto -s -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0")
 
       # string(APPEND CMAKE_CXX_LINKER_FLAGS " -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wl,-Bsymbolic")
       # string(APPEND CMAKE_CXX_LINKER_FLAGS_DEBUG "")
